b767d7efd840b947b472bc2499a3ab0b70b3c126
Carlos Morocho <47749329+cmorocho-new@users.noreply.github.com>
2020-05-21 13:01:44 -0500

    Cambio metodo de similitud

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__)
 
 
-def calcular_similitud_jaccard(nuevo_vino):
+def calcular_similitud_jaccard(valores_vino):
+    """
+    Metodo que recoree el data set y calcula la similitud con el nuevo vino
+    :param valores_vino:
+    :return:
+    """
+    def calculo_similitud(calidad, set_valores_vino):
+        """
+        Reliza el calculo de similitud de jaccard
+        :param calidad:
+        :param set_valores_vino:
+        :return:
+        """
+        return {
+            'calidad': calidad,
+            'similitud': len(set_valores_vino.intersection(valores_vino)) /
+                         len(set_valores_vino.union(valores_vino))
+        }
+
     lista_calculada = []
     with open("winequality-red.csv", "r") as f:
-        linea = f.readline()
-        while linea:
-            linea = f.readline()
-            propiedades_vino = linea.split(";")
-            set_vino = set(map(float, propiedades_vino[:-1]))
-            lista_calculada.append({
-                'calidad': propiedades_vino[-1:][0].replace("\n", ""),
-                'similitud': len(set_vino.intersection(nuevo_vino)) / len(set_vino.union(nuevo_vino))
-            })
-    # Retorna los 15 mas similares
-    return sorted(lista_calculada, key=lambda item: item['similitud'], reverse=True)[:16]
+        valores_vino_data = f.readline()
+        while valores_vino_data:
+            valores_vino_data = f.readline()
+            propiedades_vino = valores_vino_data.split(";")
+            calidad = propiedades_vino[-1:][0].replace("\n", "")
+            lista_calculada.append(calculo_similitud(calidad, set(map(float, propiedades_vino[:-1]))))
+    # Retorna las similitudes y su calidad
+    return sorted(lista_calculada, key=lambda item: item['similitud'], reverse=True)
+
+
+@app.route('/calculo_similitud', methods=['POST'])
+def calcular_calidad():
+    valores_vino = list(map(float, request.form.values()))
+    return render_template("index.html", valores=valores_vino, lista=calcular_similitud_jaccard(valores_vino))
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
-
-
-@app.route('/comparacion', methods=['POST'])
-def calcular_calidad():
-    valores = [float(num) for num in request.form.values() if num]
-    return render_template("index.html", lista=calcular_similitud_jaccard(valores))
 
 
 if __name__ == '__main__':
